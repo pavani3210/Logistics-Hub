3fe7f9e2e9f3b6314707f4272d509fadd3deb7ea
Pavani Rangineni <ranginenipavani789@gmail.com>
2023-10-19 14:40:46 -0500

    login

--- src/components/LoginSignUp.jsx
+++ src/components/LoginSignUp.jsx
@@ -46,16 +46,9 @@ const LoginSignUp = () => {
         : 
         <HomePageNav/> 
       }
-      <div className='container' style={{background:"black"}}>
+      <div className='container'>
           <div className='header'>
               <div className='text'>{action}</div>
-          </div>
-          <div className='inputs'>
-              
-                  {action==="Login"?<div></div>:<div className="input">
-                  <img class="logo-container" src={user_icon} alt=""/>
-                  <input type="text" placeholder='Name'  />
-              </div>}
           </div>
           <div className='inputs'>
               <div className="input">
